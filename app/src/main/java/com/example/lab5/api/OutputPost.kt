8a6e1e47dc2ee71ed70c0c758bd08872fa16a4f9
Gonçalo Quesado <goncaloquesado@gmail.com>
2023-05-05 12:24:45 +0100

    Commit 1 - Lab 5 finalizado

---
+++ app/src/main/java/com/example/lab5/api/OutputPost.kt
@@ -0,0 +1,6 @@
+package com.example.lab5.api
+
+data class OutputPost(
+    val id: Int,
+    val title: String
+)
---
+++ app/src/main/java/com/example/lab5/api/User.kt
@@ -0,0 +1,16 @@
+package com.example.lab5.api
+
+data class User(
+    val id: Int,
+    val name: String, val email: String, val address: Address
+)
+data class Address(
+    val city: String, val zipcode: String,
+    val company: Company,
+    val geo: Geo
+)
+data class Geo (
+    val lat: String,
+    val lng: String
+)
+data class Company ( val name: String )
